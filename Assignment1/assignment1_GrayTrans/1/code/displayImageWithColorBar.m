b92e299614892f0326bdbdd9e344bbde1138bbeb
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2015-08-15 17:42:09 +0530

    Documented myMainScript for problem1

---
+++ Assignment1/assignment1_GrayTrans/1/code/displayImageWithColorBar.m
@@ -0,0 +1,15 @@
+function displayImageWithColorBar(image, image_title, color_scale, map)
+% displayImageWithColorBar displays image with color bar 
+%   *Arguments*:
+%       * image: Image you is going to be displayed
+%       * color_scale: Color scale for colormap
+%       * map: colormap name ex. gray, jet
+    imshow(image, []);
+    axis on;
+    title(image_title, 'FontSize', 10);
+    os2 = get(gca, 'Position');
+    colormap(color_scale);
+    colormap(map);
+    daspect([1 1 1]);
+    colorbar;
+    set(gca, 'position', os2);
--- Assignment1/assignment1_GrayTrans/1/code/myMainScript.m
+++ Assignment1/assignment1_GrayTrans/1/code/myMainScript.m
@@ -1,17 +1,54 @@
-%% MyMainScript
+%% Image Resizing
+% Objective:
+%
+% * Shrinking image to show Moire effect on concentric circles
+% * Image enlargement using bilinear interpolation
+% * Image enlargement using nearest neighbour interpolation
+%
 
-tic;
-%% Your code here
+%% Colormap
+% * Number of intensities = 256
+% * displayGrayScale is a lambda function which displays an image with
+% 256 intensities
+
+myNumOfColors= 256;
+myColorScale = [ [0:1/(myNumOfColors-1):1]' , [0:1/(myNumOfColors-1):1]' , [0:1/(myNumOfColors-1):1]' ];
+displayGrayScale = @(image, title) displayImageWithColorBar(image, title, myColorScale, gray);
+
+%% Defining and Reading test input files
+
 barbara_filename = '../data/barbaraSmall.png';
 circles_filename = '../data/circles_concentric.png';
 
 barbara = imread(barbara_filename);
 circles = imread(circles_filename);
+%% Image shrinking
 
+%%
+% *Original Image*
+displayGrayScale(circles, 'original circles');
+
+%%
+% *Shrinked by factor of 2*
 shrink_by_2 = myShrinkImageByFactorD(circles, 2);
+displayGrayScale(shrink_by_2, 'shrinked by factor of 2');
+
+%%
+% *Shrinked by factor of 3*
 shrink_by_3 = myShrinkImageByFactorD(circles, 3);
+displayGrayScale(shrink_by_3, 'shrinked by factor of 3');
 
+%% Image Enlargement
+% *Original Image*
+displayGrayScale(barbara, 'Original Barbara');
+
+%%
+% *Bilinear Interpolaion*
 bilinear_interpolation = myBilinearInterpolation(barbara);
-nearest_neighbour_interpolation = myNearestNeighborInterpolation(barbara);
+displayGrayScale(bilinear_interpolation, 'barbara with bilinear interpolation');
 
-toc;
+%%
+% *Nearest Neighbour Interpolation*
+nearest_neighbour_interpolation = myNearestNeighborInterpolation(barbara);
+displayGrayScale(nearest_neighbour_interpolation, 'barbara with nearest neighbour interpolation');
+
