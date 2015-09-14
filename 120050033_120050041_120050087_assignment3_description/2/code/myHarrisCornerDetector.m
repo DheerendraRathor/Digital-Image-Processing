dd32fe2cc28f67e9d6489935197147b7bd9c9517
bijoysingh <bijoysingh693@gmail.com>
2015-09-14 23:11:32 +0530

    Cleanup code

--- 120050033_120050041_120050087_assignment3_description/2/code/myHarrisCornerDetector.m
+++ 120050033_120050041_120050087_assignment3_description/2/code/myHarrisCornerDetector.m
@@ -1,35 +1,37 @@
-function corner_detected_image = myHarrisCornerDetector(image, sigma, window, k)
+function harris = myHarrisCornerDetector(image, sigma, window, k)
     [rows, cols] = size(image);
        
-    corner_detected_image = zeros(rows, cols);
+    harris = zeros(rows, cols);
     
     rescaled_image = myIntensityRescale(image, 0.0, 1.0);    
-    [g_x, g_y] = imgradientxy(rescaled_image);
+    [gx, gy] = imgradientxy(rescaled_image);
     
-    gaussian_mat = fspecial('gaussian', window, sigma);
+    g_mat = fspecial('gaussian', window, sigma);
     wb = waitbar(0, 'Harris Corner Detection Happening');
     for i = 1:rows
         for j = 1:cols
             point = [i, j];
-            [rmin, rmax, cmin, cmax] = myGetWindowBoundaries(image, point, window);
-            gaussian_clipped = myGetExactWindow(gaussian_mat, ceil(window/2), [rmax - rmin, cmax - cmin]); 
+            [r, R, c, C] = myGetWindowBoundaries(image, point, window);
+            g_clip = myGetExactWindow(g_mat, ceil(window/2), [R - r, C - c]); 
            
-            Ix_2_weighted_sum = sum(sum(gaussian_clipped.*(g_x(rmin:rmax, cmin:cmax).^2)));
-            Iy_2_weighted_sum = sum(sum(gaussian_clipped.*(g_y(rmin:rmax, cmin:cmax).^2)));
-            Ixy_weighted_sum = sum(sum(gaussian_clipped.*(g_x(rmin:rmax, cmin:cmax).*g_y(rmin:rmax, cmin:cmax))));
+            gx2 = sum(sum(g_clip.*(gx(r:R, c:C).^2)));
+            gy2 = sum(sum(g_clip.*(gy(r:R, c:C).^2)));
+            gxy = sum(sum(g_clip.*(gx(r:R, c:C).*gy(r:R, c:C))));
 
-            structure_tensor = [Ix_2_weighted_sum, Ixy_weighted_sum; Ixy_weighted_sum, Iy_2_weighted_sum];
-            corner_detected_image(i, j) = sign( det(structure_tensor) - k*(trace(structure_tensor).^2) );
+            A = [gx2, gxy; gxy, gy2];
+ 
+            harris(i, j) = sign( det(A) - k*(trace(A).^2) );
         end
         waitbar(i/rows);
     end
     close(wb);
       
-    images = zeros(rows, cols, 4);
+    images = zeros(rows, cols, 5);
     images(:, :, 1) = image;
-    images(:, :, 2) = corner_detected_image;
-    images(:, :, 3) = g_x;
-    images(:, :, 4) = g_y;
+    images(:, :, 2) = rescaled_image;
+    images(:, :, 3) = harris;
+    images(:, :, 4) = gx;
+    images(:, :, 5) = gy;
    
     myShowImages(images, 'Harris Corner Boat');
 end
