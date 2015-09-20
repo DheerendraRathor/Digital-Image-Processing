0c688e3a94e4a02461b86e833419ad9a69e97c9c
bijoy <bijoysingh693@gmail.com>
2015-09-20 21:17:55 +0530

    Temp

--- 120050033_120050041_120050087_assignment3_description/3/code/myMainScript.m
+++ 120050033_120050041_120050087_assignment3_description/3/code/myMainScript.m
@@ -1,6 +1,21 @@
-%% MyMainScript
+%% Mean Shift Image Segmentation
+% Mean shift Image Segmentation
 
-tic;
-%% Your code here
+%%  Initialization
+image = imread('../data/baboonColor.png');
+small_image = imresize(image, 0.5);
+[rows, cols] = size(small_image);
 
-toc;
+%% Image Segmentation
+segmented_image = myMeanShiftSegmentation(small_image, [21 21], [15 25], 5, 0.01);
+
+images = zeros(rows, cols, 2);
+images(:, :, 1) = small_image;
+images(:, :, 2) = segmented_image;
+
+myShowImages(images, 'Segmented Images');
+
+%% Parameters
+display(sprintf('Kernel bandwidth for color feature %d', 25));
+display(sprintf('Kernel bandwidth for spatial feature %d', 21));
+display(sprintf('Number of iterations %d', 5));
