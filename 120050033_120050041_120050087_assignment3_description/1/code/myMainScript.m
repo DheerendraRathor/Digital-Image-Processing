5bc0ea54ebde74cf9378d2cd81a952042ba663b4
bijoysingh <bijoysingh693@gmail.com>
2015-09-20 21:53:45 +0530

    pulling stuff

--- 120050033_120050041_120050087_assignment3_description/1/code/myMainScript.m
+++ 120050033_120050041_120050087_assignment3_description/1/code/myMainScript.m
@@ -7,26 +7,5 @@ addpath('../../common/');
 load ../data/boat.mat;
 [rows, cols] = size(imageOrig);
 
-noisy_image = myGaussianNoiser(imageOrig, 0.10);
-
-D0 = 97;
-filtered_image = myButterworthFiltering(noisy_image, D0, 2);
-rmsd = myRMSDofImage(imageOrig, filtered_image);
-display(rmsd);
-
-filtered_image = myButterworthFiltering(noisy_image, 0.95*D0, 2);
-rmsd = myRMSDofImage(imageOrig, filtered_image);
-display(rmsd);
-
-filtered_image = myButterworthFiltering(noisy_image, 1.05*D0, 2);
-rmsd = myRMSDofImage(imageOrig, filtered_image);
-display(rmsd);
-
-images = zeros(rows, cols, 3);
-images(:, :, 1) = imageOrig;
-images(:, :, 2) = noisy_image;
-images(:, :, 3) = filtered_image;
-
-myShowImages(images, 'Butterworth Smoothened Boat');
-
+display(myFourierEnergyRadii(imageOrig, 2));
 toc;
